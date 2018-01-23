2a7b8c7069f21647af141f6b006c1ab3f7f2df30
Kate McKenzie <kate.j.mckenzie@gmail.com>
2018-01-23 14:30:54 +1300

    Plot colour; reactive missing plot.

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(shiny)
 library(ggplot2)
 
 MAXDEPTH = 84
+COLOR = "#2b3e50"
 
 # Read data and perform once-per-process manipulation
 depth    <- read.delim("data/depth.tsv") 
@@ -10,17 +11,21 @@ miss     <- read.delim("data/miss.tsv")
 het      <- read.delim("data/het.tsv")
 het$Indv <- as.factor(het$Indv)
 
-hetMeans <- colMeans(het[-1])
-hetMeans <- data.frame("Depth" = seq_along(hetMeans) - 1, 
-                       "Mean_F" = hetMeans)
+# hetMeans <- colMeans(het[-1])
+# hetMeans <- data.frame("Depth" = seq_along(hetMeans) - 1, 
+                       # "Mean_F" = hetMeans)
+hetStats <- data.frame("Depth" = seq_along(het[-1]) - 1, 
+                         "Mean_F" = colMeans(het[-1]),
+                         "SD_F" = sapply(het[-1], sd))
 
 server <- function(input, output) {
-   
+  # output$meanHet <- hetStats$Mean_F[30]  
+  # output$foo <- "test"
   # Render step plot of genotype depths 
   # Include vertical line at selected minimum
   output$depthPlot <- renderPlot({
       ggplot(depth, aes(x = Depth, y = Freq / 1000)) +
-        geom_step() +
+        geom_step(color = COLOR) +
         xlim(0, MAXDEPTH) +
         geom_vline(xintercept = input$minDepth) +
         ggtitle("Genotype Depth Distribution") +
@@ -32,8 +37,8 @@ server <- function(input, output) {
   # threshold
   output$hetPlot <- renderPlot({
     ggplot(data = het, 
-           aes(x = Indv, y = het[, input$minDepth + 2])) + 
-        geom_bar(stat = "identity") +
+           aes(x = Indv, y = het[, input$minDepth + 2])) +
+        geom_bar(stat = "identity", fill = COLOR) +
         ylim(-0.1, 0.1) +
         ggtitle("Heterozygosity By Sample") +
         xlab("Sample Identifier") +
@@ -41,8 +46,8 @@ server <- function(input, output) {
    })
   
   output$meanHetPlot <- renderPlot({
-      ggplot(hetMeans, aes(x = Depth, y = Mean_F)) +
-        geom_step() +
+      ggplot(hetStats, aes(x = Depth, y = Mean_F)) +
+        geom_step(color = COLOR) +
         geom_vline(xintercept = input$minDepth) +
         xlim(0, MAXDEPTH) +
         ggtitle("Mean Heterozygosity By Depth Threshold") +
@@ -51,9 +56,10 @@ server <- function(input, output) {
    })
   
   output$missPlot <- renderPlot({
-      ggplot(miss, aes(x = N_Smp, y = miss[3])) +
-        geom_step() +
-        # xlim(0, MAXDEPTH) +
+      ggplot(miss, 
+        aes(x = N_Smp, y = miss[, input$minDepth + 3])) + 
+        geom_step(color = COLOR) +
+        xlim(0, 12) +
         # geom_vline(xintercept = input$minDepth) +
         ggtitle("Missing Site Distribution") + 
         xlab("Maximum Allowed Missing Samples") +
--- ui.R
+++ ui.R
@@ -1,31 +1,47 @@
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Impact of Varying Minimum Read Depth"),
+# ui <- fluidPage(theme = shinytheme("flatly"),
+# ui <- fluidPage(theme = "superhero.css",
+#        background-color:#003d3d;
+# theme background is #2b3e50
+ui <- fluidPage(theme = shinytheme("superhero"),
+  #tags$head(
+  #  tags$style(HTML("
+  #    body {
+  #      background-color:#093434;
+  #    }
+  #  "))
+  # ),
+  # Application title
+  titlePanel("Impact of Varying Minimum Read Depth"),
    
    # Sidebar with a slider input for minimum depth
    # The average depth is 29.59
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("minDepth",
-                     "Minimum genotype depth",
-                     min = 0,
-                     max = 84,
-                     value = 30)
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("minDepth",
+                  "Minimum genotype depth",
+                  min = 0,
+                  max = 84,
+                  value = 30),
+      p("Heterozygosity"),
+      p(paste("Mean", 40, ", Std Dev", 3.2))
+      # hetStats$Mean_F[30]
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("depthPlot"),
-         plotOutput("missPlot"),
          tabsetPanel(type = "tabs",
-            tabPanel("Sample Heterozygosity", 
+            tabPanel("Min Depth",
+                     plotOutput("depthPlot")),
+            tabPanel("Sample Het",
                      plotOutput("hetPlot")),
-            tabPanel("Mean Heterozygosity", 
-                     plotOutput("meanHetPlot"))
+            tabPanel("Mean Het",
+                     plotOutput("meanHetPlot")),
+            tabPanel("Missing Sites",
+                     plotOutput("missPlot"))
          )
       )
    )
